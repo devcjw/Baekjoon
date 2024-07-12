d61fdc335c295234890a13b63ec91fbe5565b24d
devcjw <devcjw@gmail.com>
2024-07-12 10:29:00 +0900

    Two dimensional array

---
+++ src/StepByStep/Step07/Prob02.java
@@ -0,0 +1,31 @@
+// 2566번 최댓값
+package StepByStep.Step07;
+
+import java.io.*;
+import java.util.*;
+
+public class Prob02 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int max = 0;  //최댓값
+        int row = 1;  //행
+        int column = 1;  //열
+
+        for (int i = 1; i <= 9; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine()," ");
+            for (int j = 1; j<= 9; j++) {
+                int num = Integer.parseInt(st.nextToken());
+                if (num > max) {
+                    max = num;
+                    row = i;
+                    column = j;
+                }
+            }
+        }
+        System.out.println(max);
+        System.out.println(row + " " + column);
+
+        br.close();
+    }
+}
