d3d8396cc27549e980b9cd7d8166adb5d3be1a23
devcjw <devcjw@gmail.com>
2024-08-30 14:44:08 +0900

    Collection of types from major company coding tests

---
+++ src/WorkBook8078/Silver/Prob18.java
@@ -0,0 +1,32 @@
+package WorkBook8078.Silver;
+
+import java.io.*;
+
+public class Prob18 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String str = br.readLine();
+        int index = 0;
+        long num = 1;
+
+        if (str.equals("0")) {
+            System.out.println(10);
+        } else {
+            while (index < str.length()) {
+                String s = String.valueOf(num);
+                if (s.contains(String.valueOf(str.charAt(index)))) {
+                    int p = 0;
+                    while (index < str.length() && p < s.length()) {
+                        if (str.charAt(index) == s.charAt(p)) index++;
+                        p++;
+                    }
+                }
+                num++;
+            }
+            System.out.println(num - 1);
+
+            br.close();
+        }
+    }
+}
